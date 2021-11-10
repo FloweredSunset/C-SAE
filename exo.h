4a0c020dd3539fa3c6a507870bf0070d84750360
FloweredSunset <92993847+FloweredSunset@users.noreply.github.com>
2021-11-10 10:13:22 +0100

    Update exo.h

--- exo.h
+++ exo.h
@@ -1,3 +1,4 @@
 int commande(int tabNum[], char tabNom[], int tabStock[], float tabPrix[], int taille, int tabInfo[]);
-void historiqueCommande(int tabInfo[], int taille);
+void historiqueCommande(int comptageH);
 int gstock(int tabNum[], int tabStock[], int taille);
+void devis(int tabNum[], char tabNom[], int tabStock[], float tabPrix[], int taille, int tabInfo[]);
